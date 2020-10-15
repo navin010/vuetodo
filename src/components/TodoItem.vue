819e8f9b0e42747edffe2d117272c1859f2202ef
navin010 <navin010>
2020-10-15 23:06:01 +0100

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>{{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props: ["todo"]
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: white;
+        padding: 10px;
+        border-bottom: 1px white dotted;
+    }
+
+    .is-complete{
+        text-decoration: line-through;
+    }
+
+    .del{
+        background: red;
+        color: white;
+    }
+
+
+</style>
